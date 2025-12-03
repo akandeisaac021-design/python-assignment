3ea25a70c1587c52d43f3cfc551df495099fe7fc
akandeisaac021-design <akandeisaac021@gmail.com>
2025-12-03 13:31:00 +0100

    Add files via upload

---
+++ LargestOfThree.py
@@ -0,0 +1,9 @@
+number1 =int(input("Enter First number: ")
+number2 =int(input("Enter second number: ")
+number3 =int(input("Enter Third number: ")
+if (number1>number2 and number1>number3);
+    :print(number1)
+elif (number2>number1 and number2>number3)
+    :print(number2)
+elif (number3>number1 and number3>number2)
+    :print(number3)
---
+++ SafeDivision.py
@@ -0,0 +1,7 @@
+first_number =int(input('Enter first number: ')
+second_number =int(input('Enter Second number: ')
+division =first_number/second_number
+if (second_number!=0):
+    :print(division)
+elif (second_number==0):
+    :print ("cannot divide by zero")
