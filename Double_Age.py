477b7c477e1e546cc20ae6b82b503ef01b41f476
akandeisaac021-design <akandeisaac021@gmail.com>
2025-12-03 12:51:02 +0100

    Add files via upload

---
+++ Double_Age.py
@@ -0,0 +1,4 @@
+father's_age =int(input("father\'s age: ")
+son's _age =int(input("son\'s age: ")
+years =father's_age-2 * son's_age
+print("When the father is years year old the he would be twice the age of the son", years)
---
+++ GradeAssement.py
@@ -0,0 +1,17 @@
+score1 =int(input("Enter First score: ")
+score2 =int(input("Enter Second score: ")
+score3 =int(input("Enter Third score: ")
+total =score1 + score2 + score3
+avg =total/3
+print("Grade")
+if (avg>90):
+    :print('A')
+elif (avg<=90 and avg>80):
+    :print('B')
+elif (avg>70 and avg<=80):
+    :print('C')
+elif (avg>=60 and avg<=70):
+    :print('D')
+elif (avg<60)
+    :print('F')
+
