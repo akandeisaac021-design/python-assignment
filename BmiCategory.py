ea6cd9f85346bd159f2d42812b9868a12383d793
akandeisaac021-design <akandeisaac021@gmail.com>
2025-12-03 13:47:32 +0100

    Add files via upload

---
+++ BmiCategory.py
@@ -0,0 +1,12 @@
+weight =int(input("Enter your weight: ")
+height =int(input("Enter your height: ")
+height_sqr =height * height
+bmi =weight/height_sqr
+if (bmi<18.5):
+    :print("Underweight")
+elif (bmi>=18.5 and bmi<=24.9):
+    :print("Normal")
+elif (bmi>=25 and bmi>=29.9)
+    :print("Overweight")
+elif (bmi>=30):
+    :print("Obese")
