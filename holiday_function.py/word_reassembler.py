1b9979ac837a0a60a1d9c7fe87cbbccf7d768fa7
akandeisaac021-design <akandeisaac021@gmail.com>
2025-12-30 17:11:28 +0100

    add

---
+++ holiday_function.py/word_reassembler.py
@@ -0,0 +1,13 @@
+def first_word =string(input("Enter first word: "))
+        if (len(first_word) <2):
+            first_letter =""   
+        elif (len(first_word) >=2):
+            first_word_letters =list(first_word)    
+                first_letter =first_word_letters(0)
+                second_letter =first_word_letters(1)
+                last_letter =first_word_letters(-1)
+                second_to_last_letter =first_word_letters(-2)
+
+
+    return (first_letter + second_letter + second_to_last_letter + last_letter )
+    
